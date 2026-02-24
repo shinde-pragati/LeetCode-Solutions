f0a5e6546b9756c071a38549cddac06b5edaeae7
pragati shinde <shindepragati308@gmail.com>
2026-02-24 14:26:26 +0530

    climbingstairs

---
+++ climbingstairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+        return climb(n, memo);
+    }
+    
+private:
+    int climb(int n, vector<int>& memo) {
+        if (n <= 2) return n;
+        if (memo[n] != -1) return memo[n];
+        
+        memo[n] = climb(n - 1, memo) + climb(n - 2, memo);
+        return memo[n];
+    }
+};
