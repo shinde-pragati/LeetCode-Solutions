8c49e58c400628a309c8289ce2ecec7a0eec546d
pragati shinde <shindepragati308@gmail.com>
2026-02-24 14:52:58 +0530

    excel sheet column title

---
+++ excelsheetcolumntitle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result = "";
+        
+        while (columnNumber > 0) {
+            columnNumber--;
+            
+  
+            int remainder = columnNumber % 26;
+            result = char('A' + remainder) + result;
+            
+            columnNumber /= 26;
+        }
+        
+        return result;
+    }
+};
