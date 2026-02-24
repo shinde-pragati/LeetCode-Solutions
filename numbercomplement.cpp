5f9b3ccc738b1955e4424fd594d26013e8b9e2b0
pragati shinde <shindepragati308@gmail.com>
2026-02-24 16:32:19 +0530

    number complement

---
+++ numbercomplement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findComplement(int num) {
+      unsigned int mask = ~0;
+      while (mask & num) {
+        mask <<= 1;
+      }  
+      return ~mask & ~num;
+    }
+};
