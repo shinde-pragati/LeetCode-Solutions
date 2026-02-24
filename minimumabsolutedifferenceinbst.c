3bc7926ec57d1ea810aff130414e9217939e1795
pragati shinde <shindepragati308@gmail.com>
2026-02-24 16:46:35 +0530

    minimum absolute difference in bst

---
+++ minimumabsolutedifferenceinbst.c
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int minDiff = INT_MAX;
+    int prevVal = -1;
+    
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        
+        inorder(root->left);
+        
+        if (prevVal != -1) {
+            minDiff = min(minDiff, root->val - prevVal);
+        }
+        prevVal = root->val;
+        
+        inorder(root->right);
+    }
+    
+public:
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
+        return minDiff;
+    }
+};
