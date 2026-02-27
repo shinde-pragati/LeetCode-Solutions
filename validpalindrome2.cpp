195cc3c5cf98e778bc56c69f5b721a9b8759a533
pragati shinde <shindepragati308@gmail.com>
2026-02-27 17:57:11 +0530

    valid palindrome 2

---
+++ validpalindrome2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            if (s[left] != s[right]) {
+                // Try skipping either left character or right character
+                return isPalindrome(s, left + 1, right) || 
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    
+private:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
