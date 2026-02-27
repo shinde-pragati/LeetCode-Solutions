c6137e70d9c42da919555a23898c77378902e88f
pragati shinde <shindepragati308@gmail.com>
2026-02-27 14:16:52 +0530

    maximum average sub array 1

---
+++ maximumaveragesubarray1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += nums[i];
+
+        }
+        int maxSum = windowSum;
+        for (int i = k; i < nums.size(); i++) {
+            windowSum = windowSum - nums[i - k] + nums[i];
+            maxSum = max(maxSum,windowSum);
+        }
+        return static_cast<double>(maxSum) / k;
+    }
+};
