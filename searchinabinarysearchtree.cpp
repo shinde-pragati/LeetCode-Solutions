3776466fbf8e8a371b8f5f3974d6291f7db7d68f
pragati shinde <shindepragati308@gmail.com>
2026-03-02 11:44:10 +0530

    search in a binary search tree

---
+++ searchinabinarysearchtree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while (root != nullptr && root->val != val) {
+            if (val < root->val) {
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+        }
+        return root;
+    }
+};
