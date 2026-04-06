6b8f6821d62750133e3b00e32ffdc06ef71a1b4b
pragati shinde <shindepragati308@gmail.com>
2026-04-06 17:36:12 +0530

    Greatest common divisior of strings

---
+++ Greatest common divisior of strings.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        int g = gcd(str1.size(), str2.size());
+        return str1.substr(0, g);
+    }
+};
